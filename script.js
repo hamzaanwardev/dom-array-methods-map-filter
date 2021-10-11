e4ed9bd658e3276472afeec60b517a5eee6b96d8
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-10-12 00:03:28 +0500

    JS Done

--- script.js
+++ script.js
@@ -49,3 +49,48 @@ function doubleMoney() {
     updateDOM();
   }
   
+// Calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+      wealth
+    )}</strong></h3>`;
+    main.appendChild(wealthEl);
+  }
+  
+  // Add new obj to data arr
+  function addData(obj) {
+    data.push(obj);
+  
+    updateDOM();
+  }
+  
+  // Update DOM
+  function updateDOM(providedData = data) {
+    // Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  
+    providedData.forEach(item => {
+      const element = document.createElement('div');
+      element.classList.add('person');
+      element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+        item.money
+      )}`;
+      main.appendChild(element);
+    });
+  }
+  
+  // Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+  function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  }
+  
+  // Event listeners
+  addUserBtn.addEventListener('click', getRandomUser);
+  doubleBtn.addEventListener('click', doubleMoney);
+  sortBtn.addEventListener('click', sortByRichest);
+  showMillionairesBtn.addEventListener('click', showMillionaires);
+  calculateWealthBtn.addEventListener('click', calculateWealth);
+  
